4194b5be5abb9a6d865b51c0e958bc8e213b50c8
julian3493 <julianramosarevalo@gmail.com>
2021-09-22 10:52:21 -0500

    install redux and react-redux packages

--- src/API/apiData.js
+++ src/API/apiData.js
@@ -2,7 +2,7 @@ const apiData = async () => {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151');
     const data = await response.json();
-    const results = { ...data.results   };
+    const results = { ...data.results };
     console.log(results);
   } catch (error) {
     console.log(error);
