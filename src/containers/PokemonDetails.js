e53ee9c8c9cb33127cd4bc9c99a7bf45750ee887
julian3493 <julianramosarevalo@gmail.com>
2021-09-28 10:33:55 -0500

    show some details in pokemon details page

--- src/containers/PokemonDetails.js
+++ src/containers/PokemonDetails.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { selectedPokemon } from '../redux/actions/actions';
 
 const PokemonDetails = () => {
   const { pokemonId } = useParams();
+  const pokemon = useSelector((state) => state.selectedPokemon);
+  const {
+    name, stats, types, height, weight,
+  } = pokemon;
+  const dispatch = useDispatch();
+  console.log(stats, types, height, weight);
+
+  const fetchSelectedPokemon = async () => {
+    const response = await axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .catch((err) => {
+        console.log('Error', err);
+      });
+    dispatch(selectedPokemon(response.data));
+  };
+
+  useEffect(() => {
+    fetchSelectedPokemon();
+  }, [pokemonId]);
 
   return (
-    <h1>
-      Pokemon page
-      {pokemonId}
-    </h1>
+    <div className="section is-medium">
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <p className="card-header-title is-capitalized">
+              #
+              {pokemonId}
+              &nbsp;
+              {name}
+            </p>
+          </div>
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img src={pokemon.sprites.front_default} alt={name} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="content">
+              <p>
+                Height:
+                {height}
+              </p>
+              <p>
+                Weight:
+                {weight}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
