ed94f0dc760804c218b2a93ec65916b7c0c156d7
julian3493 <julianramosarevalo@gmail.com>
2021-09-29 19:19:41 -0500

    move app.css and index.css to styles folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import '../App.css';
+import '../styles/App.css';
 import Routes from './Routes';
 
 const App = () => (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './redux/store';
