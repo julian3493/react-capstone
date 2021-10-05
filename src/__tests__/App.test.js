ac2f0f160e4df418fc5596020c7959dfb67b1437
julian3493 <julianramosarevalo@gmail.com>
2021-10-04 19:12:56 -0500

    fix app.test issue

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,8 +2,8 @@ import renderer from 'react-test-renderer';
 import App from '../components/App';
 
 it('it tests the render App correctly', () => {
-  const App = renderer
+  const app = renderer
     .create(<App />)
     .toJSON();
-  expect(App).toMatchSnapshot();
+  expect(app).toMatchSnapshot();
 });
