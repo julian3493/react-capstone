29465852a029ccb2b17e6111b79dfd5f1aad859e
julian3493 <julianramosarevalo@gmail.com>
2021-10-05 15:06:47 -0500

    add more test cases in PokemonDetails.test.js

--- src/__tests__/PokemonDetail.test.js
+++ src/__tests__/PokemonDetail.test.js
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import PokemonDetails from '../containers/PokemonDetails';
 import store from '../redux/store';
 
@@ -15,4 +16,57 @@ it('it tests the render of pokemon details page correctly', () => {
     )
     .toJSON();
   expect(pokemondetails).toMatchSnapshot();
+});
+
+describe('render correctly the pokemon card details', () => {
+  it('renders an element card', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PokemonDetails />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const pokemonCardDetails = document.querySelector('.card');
+    expect(pokemonCardDetails).toBeDefined();
+  });
+
+  it('renders a header in the card', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PokemonDetails />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const pokemonCardDetails = document.querySelector('.card');
+    const cardHeader = document.querySelector('.card-header');
+    expect(pokemonCardDetails.firstElementChild).toEqual(cardHeader);
+  });
+
+  it('renders an image container in the card', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PokemonDetails />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const pokemonCardDetails = document.querySelector('.card');
+    const cardImage = document.querySelector('.card-image');
+    expect(pokemonCardDetails.children[1]).toEqual(cardImage);
+  });
+
+  it('renders a content in the card', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <PokemonDetails />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const pokemonCardDetails = document.querySelector('.card');
+    const cardContent = document.querySelector('.card-content');
+    expect(pokemonCardDetails.children[2]).toEqual(cardContent);
+  });
 });
