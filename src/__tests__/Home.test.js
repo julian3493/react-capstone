aa0cd357d4cc229bcc164d27a0555d981e5234ac
julian3493 <julianramosarevalo@gmail.com>
2021-10-05 14:40:35 -0500

    add more test cases in home.test.js

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import Home from '../containers/Home';
 import store from '../redux/store';
 
@@ -15,4 +16,54 @@ it('it tests the render of home page correctly', () => {
     )
     .toJSON();
   expect(home).toMatchSnapshot();
+});
+
+describe('render correctly the navbar componet', () => {
+  it('render the navbar brand', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const navbar = document.querySelector('.navbar');
+    expect(navbar.firstElementChild).toHaveClass('navbar-brand');
+  });
+  it('render the navbar select', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const navbar = document.querySelector('.navbar');
+    expect(navbar.lastElementChild).toHaveClass('navbar-menu');
+  });
+});
+
+describe('render correctly the grid', () => {
+  it('renders the grid container', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const gridContainer = document.querySelector('.is-ancestor');
+    expect(gridContainer).toBeDefined();
+  });
+  it('renders the grid', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const gridContainer = document.querySelector('.is-ancestor');
+    expect(gridContainer.firstElementChild).toHaveClass('tile is-parent');
+  });
 });
